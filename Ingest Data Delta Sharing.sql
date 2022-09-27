fe262655f8c5d9a52687f48f1a45bb45cb52dc59
youssef.mrini@hotmail.fr <youssef.mrini@hotmail.fr>
2022-09-27 15:15:21 +0000

    Delta Sharing

---
+++ Ingest Data Delta Sharing.sql
@@ -0,0 +1,58 @@
+-- Databricks notebook source
+SET spark.databricks.delta.formatCheck.enabled=false
+
+-- COMMAND ----------
+
+create catalog demo_deltasharing;
+use catalog demo_deltasharing;
+create database ingestion;
+use ingestion;
+
+--create table mutualfund using delta as select * from csv."abfss://songkun-uc-external-2@songkunucexternal.dfs.core.windows.net/MutualFund.csv" 
+
+-- COMMAND ----------
+
+-- MAGIC 
+-- MAGIC %python
+-- MAGIC df=spark.read.format("csv").option("header","true").option("inferSchema","true").load("abfss://songkun-uc-external-2@songkunucexternal.dfs.core.windows.net/MutualFund.csv")
+-- MAGIC df.write.saveAsTable("mutualfund")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC df=spark.sql("select quarter(price_date) as quarter, year(price_date) as year, month(price_date) as month, to_date(price_date) as date, fund_symbol, nav_per_share from mutualfund")
+-- MAGIC df.write.partitionBy("year","quarter","month").saveAsTable("mutual_fund")
+
+-- COMMAND ----------
+
+describe extended mutual_fund
+
+-- COMMAND ----------
+
+ALTER TABLE mutual_fund SET TBLPROPERTIES (delta.enableChangeDataFeed = true)
+
+-- COMMAND ----------
+
+select * from mutual_fund
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DateType, DoubleType
+-- MAGIC 
+-- MAGIC 
+-- MAGIC data2 = [(2,1974,8,"1974-08-01","ACGIX",5.62),
+-- MAGIC     (2,1974,8,"1974-08-02","ACGIX",5.0),
+-- MAGIC     (3,1974,9,"1974-08-03","ACGIX",4000.0),
+-- MAGIC     (3,1974,10,"1974-08-04","ACGIX",4000.0),
+-- MAGIC     (4,1974,8,"1974-08-05","ACGIX",1.0)]
+-- MAGIC 
+-- MAGIC df = spark.createDataFrame(data2,["quarter","year","month","date","fund_symbol","nav_per_share"])
+-- MAGIC df.createOrReplaceTempView("updates")
+-- MAGIC display(df)
+
+-- COMMAND ----------
+
+merge into mutual_fund using updates 
+on mutual_fund.date=updates.date and mutual_fund.fund_symbol=updates.fund_symbol
+WHEN MATCHED THEN UPDATE SET *
--- Managed Delta Sharing (Producer).sql
+++ Managed Delta Sharing (Producer).sql
@@ -1,44 +1,54 @@
 -- Databricks notebook source
-drop share boat_share;
-drop recipient youssef_boat_share;
+drop share finance;
+drop recipient consumer;
 
 -- COMMAND ----------
 
-CREATE SHARE IF NOT EXISTS boat_share
+CREATE SHARE IF NOT EXISTS finance
 COMMENT 'Delta Sharing  for youssef';
 
 -- COMMAND ----------
 
-DESCRIBE SHARE boat_share;
+DESCRIBE SHARE finance;
 
 -- COMMAND ----------
 
-use catalog disaster;
-use boat;
+use catalog demo_deltasharing;
+use ingestion;
 --CREATE TABLE   disaster.boat.titanics AS SELECT * FROM hive_metastore.default.titanic;
 
 -- COMMAND ----------
 
-describe history disaster.boat.titanics 
+alter share finance  
+ADD TABLE mutual_fund
+COMMENT "Change Data Feed"
+WITH CHANGE DATA FEED;
 
 -- COMMAND ----------
 
-ALTER SHARE boat_share ADD TABLE disaster.boat.titanics
+ALTER SHARE finance REMOVE TABLE demo_deltasharing.ingestion.mutual_fund;
+
 
 -- COMMAND ----------
 
-SHOW ALL IN SHARE boat_share;
+ALTER SHARE finance
+ADD TABLE mutual_fund
+PARTITION (year = "2020", month = "8");
 
 -- COMMAND ----------
 
-CREATE RECIPIENT IF NOT EXISTS studio USING ID 'aws:us-west-2:171cd945-ec1f-47c4-ad88-a37afd9c0f4a';
+SHOW ALL IN SHARE finance;
 
 -- COMMAND ----------
 
-GRANT SELECT ON SHARE boat_share TO RECIPIENT studio;
+CREATE RECIPIENT IF NOT EXISTS consumer USING ID 'aws:us-west-2:171cd945-ec1f-47c4-ad88-a37afd9c0f4a';
+
+-- COMMAND ----------
+
+GRANT SELECT ON SHARE finance TO RECIPIENT consumer;
 
 -- COMMAND ----------
 
 REVOKE SELECT
-ON SHARE boat_share
-FROM RECIPIENT studio;
+ON SHARE finance
+FROM RECIPIENT consumer;
