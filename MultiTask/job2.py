33763d59ce3a81925f49158eb3d892e7f8be05d2
youssef.mrini@hotmail.fr <youssef.mrini@hotmail.fr>
2023-03-08 13:38:39 +0000

    update

--- MultiTask/job2.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-dbutils.fs.ls("dbfs:/user/hive/warehouse/predictions")
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC 
-# MAGIC optimize delta.`dbfs:/user/hive/warehouse/predictions` zorder by predictions
--- MultiTask/job3.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-from delta import *
-df=spark.read.format("delta").load("dbfs:/user/hive/warehouse/predictions")
-df.createOrReplaceTempView("donne")
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select count(*) as sum from donne
